cecca858a27dacdb0a997e446fdc48bfa1d7e86c
NGiudiDev <ngiudice.dev@gmail.com>
2026-01-20 19:54:41 -0300

    add logic in tab

--- src/modules/common/ui/TabBar.tsx
+++ src/modules/common/ui/TabBar.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+
 export function TabBar() {
+  const pathname = usePathname();
+
   return (
     <div className="h-9 bg-[#252526] flex items-center border-b border-[#2d2d2d] overflow-x-auto">
       <div className="flex items-center gap-2 px-4 h-full cursor-pointer border-r border-[#2d2d2d] min-w-fit group bg-[#1e1e1e] text-white border-t-2 border-t-[#007acc]">
-        <span className="text-sm">page.tsx</span>
+        <span className="text-sm">
+          {(pathname).replace(/\//g, "") || "home"}.tsx
+        </span>
       </div>
     </div>
   );
